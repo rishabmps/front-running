c7c3a52e63c81012cebeb5f8d172714eee3aa51e
02himalika <himalika.singh.ece12@itbhu.ac.in>
2017-08-28 16:02:01 +0530

    trade_entity

--- src/main/java/Entities/Trade.java
+++ src/main/java/Entities/Trade.java
@@ -1,6 +1,14 @@
 package Entities;
 
+import org.joda.time.DateTime;
+
 public class Trade {
-	int tradeId;
-	String orderType;
+	int TradeId;
+	int CustomerID;
+	String TradeType;
+	String SecurityType;
+	String SecurityName;
+	DateTime time;
+	int Price;
+	float Quantity;	
 }
