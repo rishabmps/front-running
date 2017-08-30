71dc8f6bc01b5d4aa846afbc689f4a35f9abf3d7
Rishabh <rishab.mps@gmail.com>
2017-08-30 15:53:41 +0530

    Polished index.jsp

---
+++ src/main/java/com/citi/temp/Demo.java
@@ -0,0 +1,39 @@
+package com.citi.temp;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.persistence.criteria.CriteriaBuilder.In;
+
+import org.joda.time.DateTime;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		DateTime date = new DateTime(new Date());
+//
+//		System.out.println(date.toDate());
+//		DateTime prevDate = date.minusMinutes(10);
+//		System.out.println(prevDate.toDate());
+//		String startDateString = "2017-08-29 14:57:48.000";
+//
+//		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//		Date startDate;
+//		try {
+//			startDate = df.parse(startDateString);
+//			String newDateString = df.format(startDate);
+//			System.out.println(newDateString);
+//			System.out.println(startDate);
+//		} catch (ParseException e) {
+//			e.printStackTrace();
+//		}
+		
+		
+		
+	}
+
+}
